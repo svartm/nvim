7ea320880122df6c50657fe8e62e318063ced817
Minna Svartback <minna.svartback@pm.me>
2023-09-28 18:19:20 +0300

    Added key maps for easier use of system clipboard.

--- lua/oexi/core/keymaps.lua
+++ lua/oexi/core/keymaps.lua
@@ -5,6 +5,16 @@ local keymap = vim.keymap -- alias for conciseness
 
 -- Some defaults recommended:
 keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
+-- Wrapping
 keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
-keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
+keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
+
 -- Add keymaps here:
+-- System clipboard key maps
+keymap.set('v', '<leader>y', '"+y', { noremap = true, silent = true })
+keymap.set('n', '<leader>y', '"+y', { noremap = true, silent = true })
+keymap.set('n', '<leader>yy', '"+yy', { noremap = true, silent = true })
+keymap.set('v', '<leader>p', '"+p', { noremap = true, silent = true })
+keymap.set('v', '<leader>P', '"+P', { noremap = true, silent = true })
+keymap.set('n', '<leader>p', '"+p', { noremap = true, silent = true })
+keymap.set('n', '<leader>P', '"+P', { noremap = true, silent = true })
